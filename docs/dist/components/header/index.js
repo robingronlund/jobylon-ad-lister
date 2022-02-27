1b5c31ff5f88b46d099682a94542898a3465d265
Robin Grönlund <robin.gronlund@gmail.com>
2022-02-27 23:45:14 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/components/header/index.js
+++ docs/dist/components/header/index.js
@@ -1,11 +1,10 @@
 import React from "../../../snowpack/pkg/react.js";
-import logo from "../../site-logo.svg.proxy.js";
 import "./header.css.proxy.js";
 export const Header = () => {
   return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("header", {
     className: "header"
   }, /* @__PURE__ */ React.createElement("img", {
-    src: logo,
+    src: "https://www.jobylon.com/hubfs/raw_assets/public/jobylon-theme/dist/assets/site-logo.svg",
     alt: "logo"
   })));
 };
