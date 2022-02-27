33b17db153a73c19be5798565592e70a172315f9
Robin Grönlund <robin.gronlund@gmail.com>
2022-02-27 23:21:44 +0100

    Update card design

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { MdLocationOn } from 'react-icons/md';
 import parse from 'html-react-parser';
+
 import type { IJobylonAd } from 'src/interface/IJobylonAd';
+import { Tag } from '../tag';
 
 import './card.css';
-import { useNavigate } from 'react-router-dom';
 
 interface CardListProps {
   job: IJobylonAd;
@@ -14,26 +17,32 @@ export const Card: React.FC<CardListProps> = ({ job }) => {
 
   return (
     <div className="card">
-      <div>
+      <div className="card-header">
         <img src={job.company.logo} alt="company logo" className="card-logo" />
+        <label htmlFor="">{job.company.name}</label>
       </div>
-      <div className="tag-wrapper">
-        <div className="tag">{job.employment_type}</div>
-        <div className="tag">{job.experience}</div>
-      </div>
-      <div>
-        <h3>{job.title}</h3>
+
+      <Tag
+        employmentType={job.employment_type}
+        experience={job.experience}
+        industry={job.company.industry}
+      />
+      <span className="location">
+        <MdLocationOn />
+
         {job.locations.map((location, index) => (
-          <p key={index} className="location">
-            {location.location.text}
-          </p>
+          <p key={index}>{location.location.text}</p>
         ))}
-      </div>
-      <div className="cta-button-wrapper">
-        <button className="cta-button">Apply now</button>
-        <button className="cta-button" onClick={() => navigate(`/${job.slug}`)}>
-          Details
-        </button>
+      </span>
+
+      <h3>{job.title}</h3>
+
+      <div className="card-body">{parse(job.company.descr)}</div>
+
+      <div className="cta-wrapper">
+        <a className="cta" href={job.urls.ad} target="_blank" rel="noreferrer">
+          Read more
+        </a>
       </div>
     </div>
   );
