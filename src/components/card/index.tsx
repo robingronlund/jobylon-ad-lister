7fca6c822e2c4edc750e0d9229fd749294274750
Robin Grönlund <robin.gronlund@gmail.com>
2022-02-27 16:24:28 +0100

    Update styling for cards

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -8,6 +8,19 @@ interface CardListProps {
 }
 
 export const Card: React.FC<CardListProps> = ({ job }) => {
-
-  return <div className="card">{job.company.name}</div>;
+  return (
+    <div className="card">
+      <img src={job.company.logo} alt="company logo" className='card-logo' />
+      <label htmlFor="">{job.company.name}</label>
+      <div className="tag-wrapper">
+        <div className="tag">{job.employment_type}</div>
+        <div className="tag">{job.experience}</div>
+        <div className="tag">{job.function}</div>
+      </div>
+      <div className="cta-button-wrapper">
+        <button className="cta-button">APPLY NOW</button>
+        <button className="cta-button">DETAILS</button>
+      </div>
+    </div>
+  );
 };
