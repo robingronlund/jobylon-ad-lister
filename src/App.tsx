50f7d0c2bab2000f65adcefc8d97838528d43964
Robin Grönlund <robin.gronlund@gmail.com>
2022-02-27 23:23:47 +0100

    Remove job-details page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { JobsOverview } from './pages/jobs-overview';
-import { JobDetails } from './pages/job-details';
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<JobsOverview />}></Route>
-        <Route path="/:slug" element={<JobDetails />}></Route>
         <Route
           path="*"
           element={
