06872dcd1ad459d3dc197f413b2aa2e459e3a0e2
Robin Grönlund <robin.gronlund@gmail.com>
2022-02-27 23:42:07 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/components/header/index.js
+++ docs/dist/components/header/index.js
@@ -1,10 +1,11 @@
 import React from "../../../snowpack/pkg/react.js";
+import logo from "../../site-logo.svg.proxy.js";
 import "./header.css.proxy.js";
 export const Header = () => {
   return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("header", {
     className: "header"
   }, /* @__PURE__ */ React.createElement("img", {
-    src: "dist/site-logo.svg",
+    src: logo,
     alt: "logo"
   })));
 };
