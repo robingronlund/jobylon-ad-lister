e938224b17ac7f785858b78201ddabf72fab5af6
Robin Grönlund <robin.gronlund@gmail.com>
2022-02-27 23:17:25 +0100

    Add tag-component

---
+++ src/components/tag/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './tag.css';
+
+interface TagProps {
+  employmentType: string;
+  experience: string;
+  industry: string;
+}
+
+export const Tag: React.FC<TagProps> = ({ employmentType, experience, industry }) => {
+  return (
+    <div className="tag-wrapper">
+      <div className="tag">{employmentType}</div>
+      <div className="tag">{experience}</div>
+      <div className="tag">{industry}</div>
+    </div>
+  );
+};
